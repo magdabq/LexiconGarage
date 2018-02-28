6bbd60caae4ccdf3db28e5621e294b7d33dcc1d9
Magdalena Bergqvist <bergqv.m@gmail.com>
2018-02-28 16:35:05 +0100

    Lagt till ny metod

--- src/Garage.java
+++ src/Garage.java
@@ -1,17 +1,40 @@
 
 public class Garage {
-	
+
 	private int maxCapacity;
-	private int nextPosition =0;
-	
-	private Vehicle[] vehiclesInGarage = new Vehicle [100];
-	
+	private int nextPosition = 0;
+
+	private Vehicle[] vehiclesInGarage = new Vehicle[100];
+
 	public void parcVehicle(Vehicle newVehicle) {
-	
-		vehiclesInGarage [nextPosition] = newVehicle;
+
+		vehiclesInGarage[nextPosition] = newVehicle;
 		nextPosition++;
+	}
+
+	public Vehicle findVehicle(String registrationNumber) {
+
+		for (int i = 0; i < nextPosition; i++) {
+
+			if (vehiclesInGarage[i].getRegistrationNumber().equals(registrationNumber)) {
+				return vehiclesInGarage[i];
+			}
+
+		}
+
+		return null;
 	}
 	
 	
-
+	
+//	public String getTypesInGarage(Vehicle vehiclesInGarage) {
+//		
+//	private String carType = 0;
+//	private String boatType = 0;
+//	
+//	for (int i = 0; i < nextPosition; i++) {
+//	
+//	
+//	}
 }
+
